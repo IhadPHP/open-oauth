b2009a9177e59dd6e4436c6372886fe4415ad0c1
shenyan <52o@qq52o.cn>
2018-07-27 09:03:32 +0800

    服务端返回code

--- src/OauthServer.php
+++ src/OauthServer.php
@@ -8,6 +8,36 @@ namespace oauth;
 
 class OauthServer
 {
+
+    /**
+     * 请求服务端接口 获取code
+     * @param $clientId 申请应用时分配的AppId
+     * @param $redirectUri 授权回调地址
+     * @param string $responseType 返回类型，默认值为code
+     * @param string $state 用于防止CSRF攻击，成功授权后回调时会原样带回
+     */
+    public function authorize($clientId,$redirectUri,$responseType = 'code',$state = '')
+    {
+        static $clientIdModel ; // 此处应为根据clientId从数据库中查到的数据
+
+        if(empty($clientId)){
+            return ['code'=>'100001','msg'=>'缺少参数client_id'];
+        }elseif (empty($clientIdModel)){
+            // 数据库查出来的数据为空，传递的参数和数据库中的不一致，则返回不存在
+            return ['code'=>'100008','msg'=>'该appid不存在'];
+        }elseif($redirectUri != $clientIdModel['redirect_uri']){
+            //加强授权回调域验证
+            return ['code'=>'100010','msg'=>'回调地址不合法'];
+        }elseif ($responseType != 'code'){
+            return ['code'=>'100000','msg'=>'缺少参数response_type或response_type非法'];
+        }
+    }
+
+    public function setCode()
+    {
+
+    }
+
     /**
      * 生成AppId：应用的唯一标识
      * @param string $prefix 前缀
@@ -26,7 +56,7 @@ class OauthServer
     }
 
     /**
-     * 生成AppKey：公钥（相当于账号）
+     * 生成AppKey：公钥
      * @return string
      */
     public static function setAppKey()
@@ -36,7 +66,7 @@ class OauthServer
     }
 
     /**
-     * 生成AppSecret：私钥（相当于密码）
+     * 生成AppSecret：私钥
      * @return string
      */
     public static function setAppSecret()
